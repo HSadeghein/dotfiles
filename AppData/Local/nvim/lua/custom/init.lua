824c49d6dec6632b4ada1b896b43d7c9ce8ff636
Hassan Sadeghein <h.sadeghein96@gmail.com>
2023-05-04 23:43:33 +0300

    Using chezmoi

---
+++ AppData/Local/nvim/lua/custom/init.lua
@@ -0,0 +1,12 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+vim.g.neovide_cursor_animation_length = 0
+
+vim.g.neovide_cursor_trail_size = 0
+
+vim.api.nvim_set_keymap('n', '<C-n>', ':noh <CR>', {silent = true})
